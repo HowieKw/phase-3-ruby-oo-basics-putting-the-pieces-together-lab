e977de432a89887b7286354a0bffe2dbacf37ade
Howard Kwon <howie.ykwon70@gmail.com>
2021-09-01 01:51:47 -0400

    commit tree 1193b9f01b4ba25ec0a9012d889a66fec8a9275c

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
+class Book
+    attr_accessor :title, :author, :page_count, :genre
 
+    def initialize(title)
+        @title = title
+    end
 
+    def properties(author, page_count, genre)
+        @author = author
+        @page_count = page_count
+        @genre = genre
+    end
+
+    def turn_page
+        page = 0
+        until page == 1
+            page += 1
+            puts "Flipping the page...wow, you read fast!"
+        end
+    end
+end
